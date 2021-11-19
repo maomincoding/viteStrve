4c15552830e9fcec3292ab33ec11f9b3026fe48e
maomincoding <17864296568@163.com>
2021-11-20 02:00:37 +0800

    v1.2.0

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { Strve } from 'strvejs';
 import template from './App.js';
 import data from './state/index.js';
-import ways from './ways/index.js';
 
 Strve('#app', {
   data,
   template,
-  ways,
 });
+
+// HMR
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
--- src/ways/index.js
+++
@@ -1,5 +0,0 @@
-import { useEvent } from 'strvejs';
-import { f1 } from './useAdd.js';
-import { f2, f3 } from './usePush.js';
-
-export default [useEvent('#btn1', 'click', f1), useEvent('#btn2', 'click', f2), useEvent('#btn3', 'click', f3)];
